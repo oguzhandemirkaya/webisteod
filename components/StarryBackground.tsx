178694703136dcc99b6e2c019e28e342a57abbe0
Oğuzhan Demirkaya <152817189+oguzhandemirkaya@users.noreply.github.com>
2025-05-14 16:04:24 +0300

    Update StarryBackground.tsx

--- components/StarryBackground.tsx
+++ components/StarryBackground.tsx
@@ -18,13 +18,13 @@ const StarryBackground = () => {
       x: Math.random() * canvas.width,
       y: Math.random() * canvas.height,
       radius: Math.random() * 1.5,
-      speedX: (Math.random() - 0.5) * 0.2, // Çok hafif yatay hareket
-      speedY: (Math.random() - 0.5) * 0.2, // Çok hafif dikey hareket
+      speedX: (Math.random() - 0.5) * 0.2, 
+      speedY: (Math.random() - 0.5) * 0.2, 
     }));
 
     const drawStars = () => {
       ctx.clearRect(0, 0, canvas.width, canvas.height);
-      ctx.fillStyle = "rgba(255, 255, 255, 0.8)"; // Hafif parlak yıldızlar
+      ctx.fillStyle = "rgba(255, 255, 255, 0.8)"; 
 
       stars.forEach((star) => {
         ctx.beginPath();
@@ -38,7 +38,7 @@ const StarryBackground = () => {
         star.x += star.speedX;
         star.y += star.speedY;
 
-        // Kenardan çıkınca tekrar içeri al
+
         if (star.x < 0) star.x = canvas.width;
         if (star.x > canvas.width) star.x = 0;
         if (star.y < 0) star.y = canvas.height;
