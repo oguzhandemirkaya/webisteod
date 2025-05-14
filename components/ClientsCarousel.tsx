de84a45a7af5db69ee72aae85056948f5fab0f00
Oğuzhan Demirkaya <152817189+oguzhandemirkaya@users.noreply.github.com>
2025-05-14 16:03:35 +0300

    Update ClientsCarousel.tsx

--- components/ClientsCarousel.tsx
+++ components/ClientsCarousel.tsx
@@ -1,8 +1,7 @@
 "use client";
 import React from "react";
 import { motion } from "framer-motion";
-import Image from "next/image"; // ✅ Next.js Image bileşeni eklendi
-
+import Image from "next/image"; 
 interface Client {
   id: string;
   logo: string;
